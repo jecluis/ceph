71803685a1a9e8cb8b0bd8f9808d96bd2fad28a9
Joao Eduardo Luis <joao@wipwd.org>
2018-10-01 19:31:26 +0100

    mgr/hue: hue lights reflect cluster status
    Because why not?
    
    Signed-off-by: Joao Eduardo Luis <joao@wipwd.org>

---
+++ src/pybind/mgr/hue/module.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import threading
+from enum import Enum
+from mgr_module import MgrModule
+
+
+class Module(MgrModule):
+
+    def __init__(self, *args, **kwargs):
+        super(Module, self).__init__(*args, **kwargs)
+
+        self._shutdown = False
+        self._event = threading.Event()
+
+    def notify(self, notify_type, notify_id):
+        if notify_type != "health":
+            return
+        health = json.loads(self.get("health")["json"])
+        self.log.debug("Received health update: {}".format(health))
+        self._event.set()
+
+    def serve(self):
+        while True:
+            self._event.wait(10)  # hardcode for now
+            self._event.clear()
+            if self._shutdown:
+                break
+
+    def shutdown(self):
+        self._shutdown = True
+        self._event.set()
