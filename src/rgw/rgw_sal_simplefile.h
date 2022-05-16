1942638a112ad71379db25a384a5511452745a1c
Joao Eduardo Luis <joao@suse.com>
2022-05-16 17:58:44 +0000

    rgw/sfs: init store if not created
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

--- src/rgw/rgw_sal_simplefile.h
+++ src/rgw/rgw_sal_simplefile.h
@@ -87,8 +87,10 @@ class SimpleFileStore : public StoreStore {
     const DoutPrefixProvider* dpp
   ) override;
   virtual void finalize(void) override;
+  void maybe_init_store();
 
   virtual const std::string get_name() const override { return "simplefile"; }
+
   virtual std::string get_cluster_id(const DoutPrefixProvider *dpp,
                                      optional_yield y) override {
     return "NA";
@@ -289,6 +291,7 @@ class SimpleFileStore : public StoreStore {
   virtual void register_admin_apis(RGWRESTMgr * mgr) override;
 
   // TODO make proper bucket path
+  std::filesystem::path meta_path() const;
   std::filesystem::path buckets_path() const;
   std::filesystem::path users_path() const;
   std::filesystem::path bucket_path(const rgw_bucket &bucket) const;
