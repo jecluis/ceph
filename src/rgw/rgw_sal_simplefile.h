wipwd/s3gw-ceph

rgw/sfs: list buckets, list objects of a bucket
Signed-off-by: Marcel Lauhoff <marcel.lauhoff@suse.com>
Signed-off-by: Joao Eduardo Luis <joao@suse.com>

src/rgw/rgw_sal_simplefile.h
@@ -543,6 +543,9 @@ class SimpleFileStore : public Store {
       std::unique_ptr<rgw::sal::Object> _head_obj, const rgw_user &owner,
       const rgw_placement_rule *ptail_placement_rule, uint64_t olh_epoch,
       const std::string &unique_tag) override;
+
+  // TODO make proper bucket path
+  std::filesystem::path bucket_path() const { return data_path; };
 };
 
 }  // namespace rgw::sal

jecluis/ceph

src/rgw/rgw_sal_simplefile.h
@@ -729,6 +729,9 @@ class SimpleFileStore : public StoreStore {
   }
 
   virtual void register_admin_apis(RGWRESTMgr * mgr) override;
+
+  // TODO make proper bucket path
+  std::filesystem::path buckets_path() const { return data_path; };
 };
 
 }  // namespace rgw::sal