wipwd/s3gw-ceph

rgw/sfs: implement some bucket and object operations
Signed-off-by: Joao Eduardo Luis <joao@suse.com>

src/rgw/store/simplefile/user.h
@@ -26,15 +26,17 @@ class SimpleFileStore;
 
 class SimpleFileUser : public User {
  private:
-  const SimpleFileStore& store;
+  SimpleFileStore *store;
 
  protected:
   SimpleFileUser(SimpleFileUser&) = default;
   SimpleFileUser& operator=(const SimpleFileUser&) = default;
 
  public:
-  SimpleFileUser(const rgw_user &_u, const SimpleFileStore& _store) : User(_u), store(_store) {}
-  SimpleFileUser(const RGWUserInfo &_i, const SimpleFileStore& _store) : User(_i), store(_store) {}
+  SimpleFileUser(const rgw_user &_u, SimpleFileStore *_store)
+    : User(_u), store(_store) {}
+  SimpleFileUser(const RGWUserInfo &_i, SimpleFileStore *_store)
+    : User(_i), store(_store) {}
   virtual ~SimpleFileUser() = default;
 
   virtual std::unique_ptr<User> clone() override {

jecluis/ceph

src/rgw/store/simplefile/user.h
@@ -26,15 +26,17 @@ class SimpleFileStore;
 
 class SimpleFileUser : public StoreUser {
  private:
-  const SimpleFileStore& store;
+  SimpleFileStore *store;
 
  protected:
   SimpleFileUser(SimpleFileUser&) = default;
   SimpleFileUser& operator=(const SimpleFileUser&) = default;
 
  public:
-  SimpleFileUser(const rgw_user &_u, const SimpleFileStore& _store) : StoreUser(_u), store(_store) {}
-  SimpleFileUser(const RGWUserInfo &_i, const SimpleFileStore& _store) : StoreUser(_i), store(_store) {}
+  SimpleFileUser(const rgw_user &_u, SimpleFileStore *_store)
+    : StoreUser(_u), store(_store) {}
+  SimpleFileUser(const RGWUserInfo &_i, SimpleFileStore *_store)
+    : StoreUser(_i), store(_store) {}
   virtual ~SimpleFileUser() = default;
 
   virtual std::unique_ptr<User> clone() override {